c3085b3b7c9dd7507de59226f65e0490e1ae8e16
Shadab Akhtar <shadab1996@gmail.com>
2021-12-09 16:18:11 +0530

    first commit

---
+++ src/main/java/com/shadabdsw/cowinlitebackend/CowinlitebackendApplication.java
@@ -0,0 +1,13 @@
+package com.shadabdsw.cowinlitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowinlitebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CowinlitebackendApplication.class, args);
+	}
+
+}
