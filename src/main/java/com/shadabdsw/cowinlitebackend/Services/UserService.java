de1d48c7fadb01d75b879acf3612a001549a481c
Shadab Akhtar <shadab1996@gmail.com>
2021-12-15 17:32:08 +0530

    added UserService

---
+++ src/main/java/com/shadabdsw/cowinlitebackend/Services/UserService.java
@@ -0,0 +1,22 @@
+package com.shadabdsw.cowinlitebackend.Services;
+
+import com.shadabdsw.cowinlitebackend.Model.User;
+import com.shadabdsw.cowinlitebackend.Repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
