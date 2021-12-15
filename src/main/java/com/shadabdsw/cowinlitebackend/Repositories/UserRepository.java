ca6fa01e273a38a15f5262ce16a83c2c271d9be4
Shadab Akhtar <shadab1996@gmail.com>
2021-12-15 17:32:16 +0530

    added UserRepository

---
+++ src/main/java/com/shadabdsw/cowinlitebackend/Repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.shadabdsw.cowinlitebackend.Repositories;
+
+import com.shadabdsw.cowinlitebackend.Model.User;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findByName(String name);
+
+    User findByPhoneNumber(String phoneNumber);
+
+
+}
