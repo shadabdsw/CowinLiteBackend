b1140768abd2fb95dc89eeac7c99273163dbc559
Shadab Akhtar <shadab1996@gmail.com>
2021-12-22 22:21:48 +0530

    added Model

--- src/main/java/com/shadabdsw/cowinlitebackend/Model/User.java
+++ src/main/java/com/shadabdsw/cowinlitebackend/Model/User.java
@@ -1,20 +1,33 @@
 package com.shadabdsw.cowinlitebackend.Model;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "CowinUsers")
 public class User {
 
+    private String _id;
     private String name;
     private String phoneNumber;
     private String password;
     private String userType;
+    private List<Member> member;
 
-    public User(String name, String phoneNumber, String password, String userType) {
+    public User() {
+    }
+
+    public User(String _id, String name, String phoneNumber, String password, String userType, List<Member> member) {
+        this._id = _id;
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.password = password;
         this.userType = userType;
+        this.member = member;
+    }
+
+    public String get_id() {
+        return _id;
     }
 
     public String getName() {
@@ -33,9 +46,14 @@ public class User {
         return userType;
     }
 
+    public List<Member> getMember() {
+        return member;
+    }
+
     @Override
     public String toString() {
-        return "User [name=" + name + ", phoneNumber=" + phoneNumber + ", password=" + password + "User Type: " + userType + "]";
+        return "User [_id=" + _id + ", name=" + name + ", phoneNumber=" + phoneNumber + ", password=" + password
+                + ", userType=" + userType + ", member=" + member + "]";
     }
     
 }
