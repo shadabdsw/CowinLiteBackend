0b4a31ba191d51034f88f3fc4e22fab6228d2486
Shadab Akhtar <shadab1996@gmail.com>
2021-12-25 01:53:34 +0530

    added getUserById and updateUser

--- src/main/java/com/shadabdsw/cowinlitebackend/Services/UserService.java
+++ src/main/java/com/shadabdsw/cowinlitebackend/Services/UserService.java
@@ -1,9 +1,14 @@
 package com.shadabdsw.cowinlitebackend.Services;
 
+import java.util.Optional;
+
 import com.shadabdsw.cowinlitebackend.Model.User;
 import com.shadabdsw.cowinlitebackend.Repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +16,8 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public User saveUser(User user) {
         return userRepository.save(user);
@@ -23,5 +30,25 @@ public class UserService {
     public User getUserByPhoneNumber(String phoneNumber) {
         return userRepository.findByPhoneNumber(phoneNumber);
     }
+
+    public Optional<User> getUserById(String _id) {
+        return userRepository.findById(_id);
+    }
+
+    // public boolean login(User user) {
+    //     if (mongoTemplate.exists(Query.query(Criteria.where("phoneNumber").is(user.getPhoneNumber())), User.class)
+    //         && mongoTemplate.exists(Query.query(Criteria.where("password").is(user.getPassword())), User.class)) {
+    //             return true;
+    //     }
+    //     return false;
+    // }
+
+    public User updateUser(User user) {
+        Optional<User> u = getUserById(user.get_id());
+        u.get().setMember(user.getMember());
+        return userRepository.save(u.get());
+    }
+
+
 
 }
