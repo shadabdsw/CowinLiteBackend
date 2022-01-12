11b9e3e147bae676cb8c13f8bb6436a4bb623dde
Shadab Akhtar <shadab1996@gmail.com>
2022-01-12 16:03:08 +0530

    added apiInfo to the swagger ui

--- src/main/java/com/shadabdsw/cowinlitebackend/SwaggerConfig.java
+++ src/main/java/com/shadabdsw/cowinlitebackend/SwaggerConfig.java
@@ -1,10 +1,15 @@
 package com.shadabdsw.cowinlitebackend;
 
+import com.google.common.collect.Lists;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.Collections;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +24,19 @@ class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.shadabdsw.cowinlitebackend"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+            "Swagger Cowin Lite API",
+            "This is the backend of Cowin Lite.",
+            "1.0",
+            "Terms of service",
+            new Contact("Shadab Akhtar", "https://github.com/shadabdsw/", "shadab.akhtar@optimizeitsystems.com"),
+            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0.html",
+            Lists.newArrayList());
     }
 
 }
