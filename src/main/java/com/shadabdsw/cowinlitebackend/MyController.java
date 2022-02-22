fbba3ffc8a9a410f1eecfe05ddfe60cbab700367
Shadab Akhtar <shadab1996@gmail.com>
2022-02-22 14:43:45 +0530

    changed return type to responseentitiy for save

--- src/main/java/com/shadabdsw/cowinlitebackend/MyController.java
+++ src/main/java/com/shadabdsw/cowinlitebackend/MyController.java
@@ -6,12 +6,15 @@ import com.shadabdsw.cowinlitebackend.Model.User;
 import com.shadabdsw.cowinlitebackend.Services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,11 +51,38 @@ public class MyController {
         return userService.login(new User(phoneNumber, password));
     }
 
-    @PostMapping("/save")
+    @GetMapping("/users/{_id}")
     @ResponseBody
-    public User saveUser(@RequestBody User user) { // return responsebody of <User>
-        return userService.saveUser(user);
+    public ResponseEntity<Object> Get(@PathVariable String _id) {
+        try {
+            Optional<User> result = userService.Get(_id);
+            if (result.isPresent()) {
+                return ResponseHandler.generateResponse("Successfully retrieved data!", HttpStatus.OK, result);
+            } else {
+                return ResponseHandler.generateResponse("Data not found!", HttpStatus.NOT_FOUND, result);
+            }
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
+
+    @PostMapping("/save")
+    public ResponseEntity<Object> saveUser(@RequestBody User user) {
+        try {
+            User result = userService.saveUser(user);
+            if (result != null) {
+                return ResponseHandler.generateResponse("User created!", HttpStatus.CREATED, result);
+            } else {
+                return ResponseHandler.generateResponse("Data not saved!", HttpStatus.NOT_FOUND, result);
+            }
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
+    }
+
+    // public User saveUser(@RequestBody User user) {
+    //     return userService.saveUser(user);
+    // }
 
     @PostMapping("/update")
     public User updateUser(@RequestBody User user) {
@@ -78,5 +108,6 @@ public class MyController {
     public User login(@RequestBody User user) {
         return userService.loginUser(user);
     }
+
     
 }
